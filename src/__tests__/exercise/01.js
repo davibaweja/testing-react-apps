aaef6cff2ca7c621fcea3ed673f3d35b3f4eae71
Davinder Singh <davibaweja@gmail.com>
2022-04-22 11:08:17 +0100

    test 1

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -13,17 +13,30 @@ global.IS_REACT_ACT_ENVIRONMENT = true
 
 test('counter increments and decrements when the buttons are clicked', () => {
   // 🐨 create a div to render your component to (💰 document.createElement)
-  //
+  const div = document.createElement('div')
   // 🐨 append the div to document.body (💰 document.body.append)
-  //
+  document.body.append(div)
   // 🐨 use createRoot to render the <Counter /> to the div
+  const root = createRoot(div)
+  act(() => root.render(<Counter />))
   // 🐨 get a reference to the increment and decrement buttons:
   //   💰 div.querySelectorAll('button')
+  const [decrement, increment] = div.querySelectorAll('button')
   // 🐨 get a reference to the message div:
   //   💰 div.firstChild.querySelector('div')
+  const message = div.firstChild.querySelector('div')
+
   //
   // 🐨 expect the message.textContent toBe 'Current count: 0'
+  expect(message.textContent).toBe('Current count: 0')
   // 🐨 click the increment button (💰 act(() => increment.click()))
+  act(() => increment.click())
+  expect(message.textContent).toBe('Current count: 1')
+  act(() => increment.click())
+  expect(message.textContent).toBe('Current count: 2')
+  act(() => decrement.click())
+  expect(message.textContent).toBe('Current count: 1')
+
   // 🐨 assert the message.textContent
   // 🐨 click the decrement button (💰 act(() => decrement.click()))
   // 🐨 assert the message.textContent
